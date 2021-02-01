1bd80de61fb7f6c3c3bc1adf6ad745b7f14b2bc9
TheDustMan <dustman8vd@gmail.com>
2021-01-31 18:19:05 -0800

    Added initial reactjs project files

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import TestElement from './components/TestElement';
+import PropAggregator from './components/PropAggregator';
+import doTick from './components/Ticker';
+import Clock from './components/Clock';
+import Clock2 from './components/Clock2';
+import ActionLink from './components/ActionLink';
+import ToggleButton from './components/ToggleButton';
+import InputIteratorButton from './components/InputIteratorButton';
+import SaveListForm from './forms/SaveListForm';
+import Clock3 from './components/Clock3';
+
+function App() {
+  return (
+    <div className="App">
+      <TestElement/>
+      <PropAggregator name="LOLOL"/>
+      <Clock/>
+      <Clock2/>
+      <Clock3/>
+      <ActionLink/>
+      <ToggleButton/>
+      <InputIteratorButton/>
+      <SaveListForm/>
+      <div id='components'>
+      {doTick()}
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Hi
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
